d4e9f7b84c7508b18d1161267f8ba7fd0e172fed
lucasmoraist <luksmnt1101@gmail.com>
2025-06-20 09:27:26 -0300

    feat: add logging to user registration and login endpoints

--- src/main/java/collabhubbr/users/controller/UserController.java
+++ src/main/java/collabhubbr/users/controller/UserController.java
@@ -4,10 +4,12 @@ import collabhubbr.users.DTO.*;
 import collabhubbr.users.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+@Log4j2
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -15,12 +17,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseNewUserDTO> register(@Valid @RequestBody RequestNewUserDTO user) {
+    public ResponseEntity<ResponseNewUserDTO> register(@Valid @RequestBody RequestUserDTO user) {
+        log.info("Received request at endpoint /api/auth/register");
         return ResponseEntity.ok().body(userService.createAccount(user));
     }
 
     @PostMapping("/login")
     public ResponseEntity<ResponseLoginDTO> login(@Valid @RequestBody RequestLoginDTO user) {
+        log.info("Received request at endpoint /api/auth/login");
         return ResponseEntity.ok().body(userService.loginAccount(user));
     }
 
